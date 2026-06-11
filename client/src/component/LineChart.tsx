c39ec97748c6451784decd3cd309ada909be22f9
ugwucollins <123670768+ugwucollins@users.noreply.github.com>
2026-06-11 01:05:50 +0100

    Refactor LineChart to use state for data management
    Updating and fixing the errors in LineChart

--- client/src/component/LineChart.tsx
+++ client/src/component/LineChart.tsx
@@ -7,52 +7,32 @@ import DateFormat from './../context/DateFormat';
 const LineChart = (
     { data }: any
 ) => {
-    const [mins, setMins] = useState<any>([])
-    const [maxs, setMaxs] = useState<any>([])
     const [days, setDays] = useState<any>([])
-    // const [dates, setDates] = useState<any>([])
-    // const [dataAll, setDataAll] = useState<any>([])
-    const dataAll: any = []
+    
+    const [dataAll,setDataAll] =useState<any>([])
 
-
+//"Max °C"
     useEffect(() => {
-        const dates = data.daily.time.map((item: any) => {
-
+        const dates = data.daily.time.map((item: any,index) => {
             return {
-                date: +DateFormat(item).dateName.slice(0, 2),
+                date: +DateFormat(item).dateName.slice(0,2),
+                Max:data.temperature_max[index],
+                Min:data.temperature_min[index],
+
             }
         })
         setDays(dates)
+        setDataAll(datas)
 
-        const maxData = data.temperature_max.map((item: any) => {
-
-            return {
-                max: item,
-            }
-        })
-        setMaxs(maxData)
-        const minData = data.temperature_min.map((item: any) => {
-            return {
-                min: item,
-            }
-        })
-        setMins(minData)
-        const arr = {
-            temperature_max: maxs, temperature_min: mins, days: days
-        }
-
-        dataAll.push(arr)
-
-
+        
     }, [data])
 
-    const datas = dataAll?.map((item: any) => {
-        const days = item.days.split(',')
+    const datas = dataAll?.map((item: any,index) => {
         console.log(item);
         return {
-            name: days,
-            pv: item.temperature_max,
-            uv: item.temperature_min,
+            date: days,
+            Max: item.max,
+            Min: item.Min,
         }
     })
     console.log(datas);
@@ -64,7 +44,7 @@ const LineChart = (
         <div style={{ width: '100%', maxWidth: '800px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}>
             <ResponsiveContainer width="100%" height="100%">
                 <RechartsLineChart
-                    data={datas}
+                    data={dataAll||datas}
                     margin={{
                         top: 5,
                         right: 0,
@@ -73,8 +53,8 @@ const LineChart = (
                     }}
                 >
                     <CartesianGrid strokeDasharray="3 3" stroke="var(--color-border-3)" />
-                    <XAxis dataKey="name" stroke="var(--color-text-3)" />
-                    <YAxis width="auto" stroke="var(--color-text-3)" />
+                    <XAxis dataKey="date" stroke="var(--bg-color)" />
+                    <YAxis width="auto" stroke="var(--bg-color)" />
                     <Tooltip
                         cursor={{
                             stroke: 'var(--color-border-2)',
@@ -87,21 +67,21 @@ const LineChart = (
                     <Legend />
                     <Line
                         type="monotone"
-                        dataKey="pv"
-                        stroke="var(--color-chart-1)"
+                        dataKey="Max"
+                        stroke="var(--secondary-color)"
                         dot={{
-                            fill: 'var(--color-surface-base)',
+                            fill: 'var(--secondary-color)',
                         }}
-                        activeDot={{ r: 8, stroke: 'var(--color-surface-base)' }}
+                        activeDot={{ r: 8, stroke: 'var(--secondary-color)' }}
                     />
                     <Line
                         type="monotone"
-                        dataKey="uv"
-                        stroke="var(--color-chart-2)"
+                        dataKey="Min"
+                        stroke="var(--primary-color)"
                         dot={{
-                            fill: 'var(--color-surface-base)',
+                            fill: 'var(--primary-color)',
                         }}
-                        activeDot={{ stroke: 'var(--color-surface-base)' }}
+                        activeDot={{ stroke: 'var(--primary-color)' }}
                     />
                     <RechartsDevtools />
                 </RechartsLineChart>
