d0a8e2d0bcb789de0cd7808e4c7f0efc506d4913
ugwucollins <123670768+ugwucollins@users.noreply.github.com>
2026-06-13 16:21:48 +0100

    Remove sample data and update AreaChart styles

--- client/src/component/AreaChart.tsx
+++ client/src/component/AreaChart.tsx
@@ -3,56 +3,14 @@ import { useEffect, useState } from 'react';
 import { Area, AreaChart, Tooltip, XAxis, YAxis } from 'recharts';
 import DateFormat from './../context/DateFormat';
 
-// #region Sample data
-// const data = [
-//     {
-//         name: 'Page A',
-//         uv: 4000,
-//         pv: 2400,
-//         amt: 2400,
-//     },
-//     {
-//         name: 'Page B',
-//         uv: 3000,
-//         pv: 1398,
-//         amt: 2210,
-//     },
-//     {
-//         name: 'Page C',
-//         uv: 2000,
-//         pv: 9800,
-//         amt: 2290,
-//     },
-//     {
-//         name: 'Page D',
-//         uv: 2780,
-//         pv: 3908,
-//         amt: 2000,
-//     },
-//     {
-//         name: 'Page E',
-//         uv: 1890,
-//         pv: 4800,
-//         amt: 2181,
-//     },
-//     {
-//         name: 'Page F',
-//         uv: 2390,
-//         pv: 3800,
-//         amt: 2500,
-//     },
-//     {
-//         name: 'Page G',
-//         uv: 3490,
-//         pv: 4300,
-//         amt: 2100,
-//     },
-// ];
+
 type AreaChartPropS = {
     isAnimationActive?: boolean,
     data: any,
 }
 // #endregion
+
+
 const AreaChartExample = ({ isAnimationActive = true, data }: AreaChartPropS) => {
     const [days, setDays] = useState<any>([])
     const [dataAll, setdataAll] = useState<any>([])
@@ -80,7 +38,7 @@ const AreaChartExample = ({ isAnimationActive = true, data }: AreaChartPropS) =>
     })
 
     return (
-        <div className='py-2 cursor-pointer my-5 px-4 bg-bg-color/5 rounded-2xl shadow-2xl drop-shadow-2xl border border-border-color/20 backdrop-blur-2xl hover:shadow-bg-color/30 transition-all duration-150'>
+        <div className='py-2 cursor-pointer w-full min-h-[65vh] my-5 px-4 bg-bg-color/5 rounded-2xl shadow-2xl drop-shadow-2xl border border-border-color/20 backdrop-blur-2xl hover:shadow-bg-color/30 transition-all duration-150'>
             <AreaChart
                 style={{ width: '100%', maxWidth: '900px', maxHeight: '60vh', aspectRatio: 1.618 }}
                 responsive
